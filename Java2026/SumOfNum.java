10ef821a6560fa85fbf68acc3d54e5e313a2295e
Perumal M <perumal182005@gmail.com>
2026-06-23 20:57:15 +0530

    first commit

---
+++ Java2026/SumOfNum.java
@@ -0,0 +1,13 @@
+public class SumOfNum
+{
+    public static void main(String[] args)
+    {
+        int sum = 0;
+        for(int i = 1; i<=10; i++)
+        {
+            sum =sum + i;
+        }
+        System.out.println("Sum of first 10 natural number is: " + sum);
+
+    }
+}
